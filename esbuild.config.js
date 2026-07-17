7c69be1efc1a5ac5a532df52258246560656c066
OwodeFood Developer <lukum122@gmail.com>
2026-07-17 07:35:52 +0100

    chore: explicitly bundle bcryptjs into server.cjs

---
+++ esbuild.config.js
@@ -0,0 +1,20 @@
+import esbuild from 'esbuild';
+import fs from 'fs';
+
+const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const dependencies = Object.keys(packageJson.dependencies);
+
+// Keep everything external EXCEPT bcryptjs so it gets bundled directly into server.cjs
+const externalDeps = dependencies.filter(dep => dep !== 'bcryptjs');
+
+esbuild.build({
+  entryPoints: ['server.ts'],
+  bundle: true,
+  platform: 'node',
+  format: 'cjs',
+  external: externalDeps,
+  sourcemap: true,
+  outfile: 'dist/server.cjs',
+}).then(() => {
+  console.log('Server bundled successfully with bcryptjs included!');
+}).catch(() => process.exit(1));
