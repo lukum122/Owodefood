d154fab634de77ec89ff04ae685b126a59851fa6
lukum122 <72826431+lukum122@users.noreply.github.com>
2026-07-31 22:22:12 +0100

    Delete temp.cjs

--- temp.cjs
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const txt = fs.readFileSync('server.ts', 'utf8'); const idx1 = txt.indexOf('case \"PRODUCT_UPSERT\":'); console.log('Line number of first occurrence:', txt.substring(0, idx1).split('\n').length);
